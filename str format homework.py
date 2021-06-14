a41ee1ce2b30310d3e0ab3a36f7ed7e70aa2e6d4
Peekar <85645128+Peekar@users.noreply.github.com>
2021-06-14 18:41:35 -0500

    str format homework

---
+++ str format homework.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+school = input("What school do you go to? ")
+grade = int(input("What grade are you in? "))
+
+if name == "Praneel" or name == "Aakash":
+    print(f"{name} is an idiot and goes to Idiot school to learn how to better be an idiot")
+else:
+    print(f"{name} goes to school at {school} to study grade {grade}.")
